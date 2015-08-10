77b24ab5c71e0546c2479b80dd15d6251eefbbea
vivian-xu <vivian.zxx53@gmail.com>
2015-08-10 17:05:09 +0800

    喜欢的菜合集

--- parse_with_re/parser.py
+++ parse_with_re/parser.py
@@ -1,50 +1,32 @@
 # -*- Encoding: utf-8 -*-
 import re
 
+food = {}
 
 def parse(content):
-    print '-------------------------------------------------'
-    print content
-
     items_ptn = re.compile(ur'<dt>(.*?)</dt>\s*<dd>(.*?)</dd>', re.DOTALL)
     items_ret = items_ptn.findall(content)
 
-    value_ptn = re.compile(ur'>([^>]*?)</', re.DOTALL)
+    value_ptn = re.compile(ur'>([^<]*?)</', re.DOTALL)
 
     for k, v in items_ret:
-        print '0'*30
-        ret = value_ptn.findall(v)
-        for i in ret:
-            print k[:-1], i
+        if k.startswith(u'喜欢的菜'):
+            ret = value_ptn.findall(v)
+            for i in ret:
+                if i not in food:
+                    food[i] = 0
+                food[i] += 1
 
-'''
-    items_ptn = re.compile(ur'<dt>(.*?)：</dt>\s*<dd>(.*?)</dd>')
-    items_ret = items_ptn.search(content)
-
-
-    key_ptn = re.compile(ur'<dt>(.*?)：</dt>')
-    key_ret = key_ptn.findall(content)
-
-
-    value_ptns = [
-        re.compile(ur'<a href.*?target="_blank">(.*?)</a>'),
-        re.compile(ur'<span class="item">(.*?)</span>')
-        ]
-
-    rets = []
-    for value_ptn in value_ptns:
-        rets.extend(value_ptn.findall(content))
-
-
-    print(','.join(key_ret))
-    print(','.join(rets))
-'''
 if __name__ == '__main__':
     import json
     data = None
     with open('data.json', 'r') as fp:
         data = json.load(fp)
 
-    num = 10
+    num = 1000
     for item in data[:num]:
         parse(item)
+
+    sort_food = sorted(food.iteritems(), key=lambda i: i[1])
+    for k, v in sort_food:
+        print k ,v
